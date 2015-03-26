425db90c653fb49bef09446709fe31cf95290278
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2015-03-26 11:56:06 +0100

    Added 'tfdt' parser

--- src/iso_box.js
+++ src/iso_box.js
@@ -268,6 +268,16 @@ ISOBox.prototype._boxParsers['tkhd'] = function() {
   this.height = this._readUint(32);
 }
 
+// ISO/IEC 14496-12:2012 - 8.8.12 Track Fragmnent Decode Time
+ISOBox.prototype._boxParsers['tfdt'] = function() {
+  this._parseFullBox();
+  if (this.version == 1) {
+    this.baseMediaDecodeTime = this._readUint(64);
+  } else {
+    this.baseMediaDecodeTime = this._readUint(32);    
+  }
+}
+
 // ISO/IEC 14496-12:2012 - 8.8.7 Track Fragmnent Header Box
 ISOBox.prototype._boxParsers['tfhd'] = function() {
   this._parseFullBox();
