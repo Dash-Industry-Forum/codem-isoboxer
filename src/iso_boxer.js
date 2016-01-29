87f6cdbb71cb59a11d5e3e3394e9282a4cde9a7d
Torbjorn Einarsson <teinarsson@mobitv.com>
2016-01-29 15:10:43 +0100

    Extended fallback UTF-8 parsing to multi-byte characters.

--- src/iso_boxer.js
+++ src/iso_boxer.js
@@ -9,11 +9,41 @@ ISOBoxer.Utils.dataViewToString = function(dataView, encoding) {
   if (typeof TextDecoder !== 'undefined') {
     return new TextDecoder(encoding || 'utf-8').decode(dataView);
   }
-  var str = '';
-  for (var i=0; i<dataView.byteLength; i++) {
-    str += String.fromCharCode(dataView.getUint8(i));
-  }    
-  return str;  
+  var a = [];
+  var i = 0;
+  if (encoding === 'utf-8') {
+      /* The following algorithm is essentially a rewrite of the UTF8.decode at 
+      http://bannister.us/weblog/2007/simple-base64-encodedecode-javascript/
+      */
+      
+      while (i < dataView.byteLength) {
+          var c = dataView.getUint8(i++);
+          if (c < 0x80) {
+              // 1-byte character (7 bits)
+          } else if (c < 0xe0) {
+              // 2-byte character (11 bits)
+              c = (c & 0x1f) << 6;
+              c |= (dataView.getUint8(i++) & 0x3f);
+          } else if (c < 0xf0) {
+              // 3-byte character (16 bits)
+              c = (c & 0xf) << 12;
+              c |= (dataView.getUint8(i++) & 0x3f) << 6;
+              c |= (dataView.getUint8(i++) & 0x3f);
+          } else {
+              // 4-byte character (21 bits)
+              c = (c & 0x7) << 18;
+              c |= (dataView.getUint8(i++) & 0x3f) << 12;
+              c |= (dataView.getUint8(i++) & 0x3f) << 6;
+              c |= (dataView.getUint8(i++) & 0x3f);
+          }
+          a.push(String.fromCharCode(c));
+      }
+  } else { // Just map byte-by-byte (probably wrong)
+      while (i < dataView.byteLength) {
+          a.push(String.fromCharCode(dataView.getUint8(i)));
+      }
+  }
+  return a.join('');  
 };
 
 if (typeof exports !== 'undefined') {
