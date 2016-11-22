110e630fcd0ae158b6808bb4aac6aa04918dea71
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-11-22 14:30:10 +0100

    avc1.js: correct/complete fields parsing and read AVCSampleEntry/AVCConfigurationRecord as data

--- src/parsers/avc1.js
+++ src/parsers/avc1.js
@@ -1,17 +1,32 @@
 // ISO/IEC 14496-15:2014 - avc1 box
 ISOBox.prototype._boxParsers['avc1'] = function() {
-  this.version               = this._readUint(16);
-  this.revision_level        = this._readUint(16);
-  this.vendor                = this._readUint(32);
-  this.temporal_quality      = this._readUint(32);
-  this.spatial_quality       = this._readUint(32);
+  // SampleEntry fields
+  this.reserved1 = [
+    this._readUint(8),
+    this._readUint(8),
+    this._readUint(8),
+    this._readUint(8),
+    this._readUint(8),
+    this._readUint(8)
+  ];
+  this.data_reference_index = this._readUint(16);
+  // VisualSampleEntry fields
+  this.pre_defined1 = this._readUint(16);
+  this.reserved2 = this._readUint(16);
+  this.pre_defined2 = [
+    this._readUint(32),
+    this._readUint(32),
+    this._readUint(32)
+  ];
   this.width                 = this._readUint(16);
   this.height                = this._readUint(16);
-  this.horizontal_resolution = this._readUint(32);
-  this.vertical_resolution   = this._readUint(32);
-  this.data_size             = this._readUint(32);
+  this.horizresolution       = this._readTemplate(32);
+  this.vertresolution        = this._readTemplate(32);
+  this.reserved3             = this._readUint(32);
   this.frame_count           = this._readUint(16);
-  this.compressor_name       = this._readUint(32);
+  this.compressorname        = this._readData(32);
   this.depth                 = this._readUint(16);
-  this.color_table_id        = this._readUint(16);
+  this.pre_defined3          = this._readInt(16);
+  // AVCSampleEntry fields
+  this.config = this._readData();
 }
