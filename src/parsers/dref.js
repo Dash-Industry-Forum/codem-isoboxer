e9dd65b53e1700ec82820e7ab5d7b41fe20b2df1
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-11-29 10:35:12 +0100

    dref: parse entries as sub-boxes

--- src/parsers/dref.js
+++ src/parsers/dref.js
@@ -3,16 +3,7 @@ ISOBox.prototype._boxParsers['dref'] = function() {
   this._parseFullBox();
   this.entry_count = this._readUint(32);
   this.entries = [];
-  for (var i=0; i<this.entry_count; i++) {
-    var entry = {};
-    entry.size = this._readUint(32);
-    entry.type = this._readString(4);
-    entry.version = this._readUint(8);
-    entry.flags = this._readUint(24);
-    if (entry.type === 'urn') {
-      entry.name = this._readTerminatedString();
-    }
-    entry.location = this._readTerminatedString();
-    this.entries.push(entry);
+  for (var i = 0; i < this.entry_count ; i++){
+    this.entries.push(ISOBox.parse(this));
   }
 };
