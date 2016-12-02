dc5f502e9fc01f15cd148f60fb44a3350692caa1
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-12-02 18:58:32 +0100

    add new lines at end of files

--- src/boxes/mfro.js
+++ src/boxes/mfro.js
@@ -2,4 +2,5 @@
 ISOBox.prototype._boxProcessors['mfro'] = function() {
   this._procFullBox();
   this._procField('mfra_size', 'uint', 32); // Called mfra_size to distinguish from the normal "size" attribute of a box
-}
+};
+
--- src/boxes/stsd.js
+++ src/boxes/stsd.js
@@ -3,11 +3,4 @@ ISOBox.prototype._boxProcessors['stsd'] = function() {
   this._procFullBox();
   this._procField('entry_count', 'uint', 32);
   this._procSubBoxes('entries', this.entry_count);
-  // Store entries as (sub)boxes when parsing.
-  // if (this._parsing) {
-  //   this.boxes = [];
-  //   for (var i = 0; i < this.entry_count; i++) {
-  //     this.boxes.push(ISOBox.parse(this));
-  //   }
-  // }
 };
