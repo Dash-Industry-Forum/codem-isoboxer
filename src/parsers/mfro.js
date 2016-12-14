8689b7e98eaef8c15834c32e11eb874112ae24d4
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-12-14 14:41:22 +0100

    Delete 'parsers' folder

--- src/parsers/mfro.js
+++
@@ -1,5 +0,0 @@
-// ISO/IEC 14496-12:2012 - 8.8.11 Movie Fragment Random Access Box
-ISOBox.prototype._boxParsers['mfro'] = function() {
-  this._parseFullBox();
-  this.mfra_size = this._readUint(32); // Called mfra_size to distinguish from the normal "size" attribute of a box
-};
--- src/parsers/url,urn.js
+++
@@ -1,8 +0,0 @@
-// ISO/IEC 14496-12:2012 - 8.7.2 Data Reference Box
-ISOBox.prototype._boxParsers['url '] = ISOBox.prototype._boxParsers['urn '] = function() {
-  this._parseFullBox();
-  if (this.type === 'urn ') {
-    this.name = this._readTerminatedString();
-  }
-  this.location = this._readTerminatedString();
-};
