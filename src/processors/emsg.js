57b08192204ee0c3af31c265d0f1318d5edbb633
J. Oliva <jesus@epiclabs.io>
2018-08-13 00:54:46 +0200

    Add support for emsg box version 1

--- src/processors/emsg.js
+++ src/processors/emsg.js
@@ -1,11 +1,20 @@
 // ISO/IEC 23009-1:2014 - 5.10.3.3 Event Message Box
 ISOBox.prototype._boxProcessors['emsg'] = function() {
   this._procFullBox();
-  this._procField('scheme_id_uri',            'string', -1);
-  this._procField('value',                    'string', -1);
-  this._procField('timescale',                'uint',   32);
-  this._procField('presentation_time_delta',  'uint',   32);
-  this._procField('event_duration',           'uint',   32);
-  this._procField('id',                       'uint',   32);
+  if (this.version == 1) {
+    this._procField('timescale',                'uint',   32);
+    this._procField('presentation_time',        'uint',   64);
+    this._procField('event_duration',           'uint',   32);
+    this._procField('id',                       'uint',   32);
+    this._procField('scheme_id_uri',            'string', -1);
+    this._procField('value',                    'string', -1);
+  } else {
+    this._procField('scheme_id_uri',            'string', -1);
+    this._procField('value',                    'string', -1);
+    this._procField('timescale',                'uint',   32);
+    this._procField('presentation_time_delta',  'uint',   32);
+    this._procField('event_duration',           'uint',   32);
+    this._procField('id',                       'uint',   32);
+  }
   this._procField('message_data',             'data',   -1);
 };
