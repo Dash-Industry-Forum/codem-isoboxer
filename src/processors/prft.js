ec7f679768f6bcd4a77a49356c67bf300183d5b4
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2023-01-04 08:49:53 +0100

    Read ntp_timestamp second and fraction since 64 bits is not supported

--- src/processors/prft.js
+++ src/processors/prft.js
@@ -2,6 +2,7 @@
 ISOBox.prototype._boxProcessors['prft'] = function() {
   this._procFullBox();
   this._procField('reference_track_ID', 'uint', 32);
-  this._procField('ntp_timestamp', 'uint', 64);
+  this._procField('ntp_timestamp_sec', 'uint', 32);
+  this._procField('ntp_timestamp_frac', 'uint', 32);
   this._procField('media_time', 'uint', (this.version == 1) ? 64 : 32);
 };
